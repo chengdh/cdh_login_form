c243fe7fd268828b2835270ad5b0ea49330a6b6e
chengdh <cheng.dongh@gmail.com>
2015-02-25 09:09:01 +0800

    change login form

---
+++ cdh_login_form.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  Plugin Name: Cheng donghui Custom Loginform
+  Plugin URI: http://wordpress.org/extend/plugins/health-check/
+  Description: custome logform
+  Author: chengdh
+  Version: 0.1-alpha
+  Author URI: http://wordpress.org/extend/plugins/health-check/
+  Text Domain: health-check
+  Domain Path: /lang
+  License: GPL2
+ */
+//define styles
+function cdh_login_form_styles() {
+  wp_register_style('cdh_login_form_styles',plugins_url('login_form.css',__FILE__));
+  wp_enqueue_style('cdh_login_form_styles');
+}
+add_action('login_enqueue_scripts', 'cdh_login_form_styles');
+
+function cdh_login_form_js() {
+  //wp_register_script('jquery',"http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js");
+  wp_register_script('cdh_login_form_js',plugins_url('login_form.js',__FILE__));
+
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('cdh_login_form_js');
+}
+
+add_action('login_enqueue_scripts', 'cdh_login_form_js');
---
+++ login_form.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+  var input_username = '<label for="user_login"><input type="text" name="log" placeholder="请输入帐号" id="user_login" class="input" value="" size="20" /></label>';
+  var input_password = '<label for="user_pass"><input type="password" name="pwd" placeholder="请输入密码" id="user_pass" class="input" value="" size="20" /></label>';
+
+  //重新构造登录界面
+  $('label[for="user_login"').parent('p').html(input_username);
+  $('label[for="user_pass"').parent('p').html(input_password);
+});
